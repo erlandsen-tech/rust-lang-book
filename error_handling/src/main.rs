98c907e4a74d9a3d2a55cd8e96d10d198da4b156
John Inge <johninge@varleg.no>
2019-04-25 07:36:01 +0200

    Error handling snippets

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,4 @@
+use std::io::ErrorKind;
 use std::fs::File;
 fn main() {
     //panic!("crash and burn!");
@@ -5,6 +6,22 @@ fn main() {
     let f = File::open("test.txt");
     let f = match f {
         Ok(file) => file,
-        Err(error) => panic!("something horrible happened while opening the file"),
+        Err(error) => panic!("something horrible happened while opening the file \n
+                             {:?}", error),
     };
+
+    let g = File::open("does_not_exist.txt");
+    //nested error
+    let g = match g {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("but_this_will.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Unable to create file \n
+                                 {:?}", e),
+            },
+            other_error => panic!("Problem opening the file {:?}", other_error),
+        },
+    };
+
 }
